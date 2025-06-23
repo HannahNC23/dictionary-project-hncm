2eacc747ed9c56c55e30d4e7f75d71a6c52afb2f
HannahNC23 <hannah.charles@boots.co.uk>
2025-06-23 09:29:47 +0100

    Adding in synonyms

---
+++ src/Synonyms.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Synonyms(props) {
+  if (props.synonyms) {
+    return (
+      <ul className="Synonyms">
+        {props.synonyms.map(function (synonym, index) {
+          return (
+            <div className="Synonyms">
+              <strong>Synonyms:</strong> <li key={index}>{synonym}</li>
+            </div>
+          );
+        })}
+      </ul>
+    );
+  } else {
+    return null;
+  }
+}
